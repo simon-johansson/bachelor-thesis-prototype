807c2b026783259f20750221c64f4de261b83e15
Simon Johansson <mail@simon-johansson.com>
2014-04-29 17:58:43 +0200

    A bunch of changes

--- js/book_list.js
+++ js/book_list.js
@@ -2,11 +2,18 @@ App = {
     books: DB.books,
     listTemplate: _.template($('#book-template').html()),
     renderBookList: function(data){
-        var $listView =  $('.list-view');
+        var $listView =  $('.book-list');
         $listView.html("");
         data.forEach(function(el){
             $listView.append(this.listTemplate(el));
         }, this);
+    },
+    highlighText: function (val) {
+        $('.book-item .title, .book-item .author').each(function(i, el){
+            var text = $(el).text();
+            text = text.toLowerCase().replace(val, ("<span class='hightlighted'>"+val+"</span>"));
+            $(el).html(text);
+        });
     },
     newSearch: function () {
         var $this = $(this),
@@ -15,17 +22,28 @@ App = {
         if(val.length > 0){
             var booksToShow = [];
             _.find(App.books, function(book){
-                if(book.title.toLowerCase().indexOf(val) > -1){
+                if( book.title.toLowerCase().indexOf(val) > -1 ||
+                    book.author.toLowerCase().indexOf(val) > -1 ){
                     booksToShow.push(book);
                 }
             });
             App.renderBookList(booksToShow);
+            App.highlighText(val);
         } else {
             App.renderBookList(App.books);
         }
     },
+    newView: function (ev) {
+        if(window.location.hash.length){
+            $('.list-view').addClass('hide');
+        } else {
+            $('.list-view').removeClass('hide');
+        }
+
+    },
     bindEvents: function(){
         $("input[type='text']").on('input change', App.newSearch);
+        $(window).on('hashchange', App.newView);
     },
     init: function(){
         this.bindEvents();
