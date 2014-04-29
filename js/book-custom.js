4dc3142424af2ef9916519145cbe7e949e11e5e0
tbruce <tor.bruce@gmail.com>
2014-04-29 16:35:38 +0200

    Kunna rotera till baksida också

--- js/book-custom.js
+++ js/book-custom.js
@@ -6,7 +6,13 @@ $(function() {
     bookTemplate = _(bookTemplate).template();
     
     var bookList = document.getElementById('bk-list'),
-        dragDistance = 0;
+        dragDistance = 0,
+        states = {
+            front: 'front',
+            back: 'back',
+            inside: 'inside',
+        },
+        state = states.front;
         
     $(bookList).append(bookTemplate({
         color: _(['gray']).sample(),
@@ -16,7 +22,17 @@ $(function() {
     Hammer(bookList).on('dragstart', function(e) {
         dragDistance = 0;
         $('.bk-book').addClass('is-dragging');
+        var $book = $('.bk-book');
+        if ($book.hasClass('bk-viewback')) {
+            state = states.back;
+        } else if ($book.hasClass('bk-viewinside')) {
+            state = states.inside;
+        } else {
+            state = states.front;
+        };
+        console.log(state);
     });
+    
     Hammer(bookList).on('dragend', function(e) {
         openBook($('.bk-book'));
     });
@@ -24,20 +40,36 @@ $(function() {
     Hammer(bookList).on('dragleft', function(e) {
         e.gesture.preventDefault();
         dragDistance = e.gesture.deltaX;
-        if (dragDistance < -240) {
-            openBook($('.bk-book'));
+        if (state === states.back) {
+            if (dragDistance < -180) {
+                openBook($('.bk-book'));
+            } else {
+                $('.bk-book').css('-webkit-transform', 'translate3d(0,0,0px) rotate3d(0,1,0,'+(180+dragDistance)+'deg)');
+            }
         } else {
-            $('.bk-bookdefault .bk-front').css('-webkit-transform', 'translate3d(0,0,20px) rotate3d(0,1,0,'+(dragDistance/3)+'deg)');
+            if (dragDistance < -240) {
+                openBook($('.bk-book'));
+            } else {
+                $('.bk-bookdefault .bk-front').css('-webkit-transform', 'translate3d(0,0,20px) rotate3d(0,1,0,'+(dragDistance/3)+'deg)');
+            }
         }
     });
     
     Hammer(bookList).on('dragright', function(e) {
         e.gesture.preventDefault();
         dragDistance = e.gesture.deltaX;
-        if (dragDistance > 180) {
-            openBook($('.bk-book'));
+        if (state === states.front) {
+            if (dragDistance > 120) {
+                openBook($('.bk-book'));
+            } else {
+                $('.bk-book').css('-webkit-transform', 'translate3d(0,0,0px) rotate3d(0,1,0,'+dragDistance+'deg)');
+            }
         } else {
-            $('.bk-viewinside .bk-front').css('-webkit-transform', 'translate3d(0,0,20px) rotate3d(0,1,0,'+(-160+dragDistance/3)+'deg)');
+            if (dragDistance > 180) {
+                openBook($('.bk-viewinside'));
+            } else {
+                $('.bk-viewinside .bk-front').css('-webkit-transform', 'translate3d(0,0,20px) rotate3d(0,1,0,'+(-160+dragDistance/3)+'deg)');
+            }
         }
     });
     
@@ -45,11 +77,16 @@ $(function() {
         $book.removeClass('is-dragging');
         setTimeout(function() {
             $book.find('.bk-front').css('-webkit-transform', '');
+            $book.css('-webkit-transform', '');
             // bestäm riktning att öppna
             if (dragDistance > 100 || (dragDistance > -100 && dragDistance < 2)) {
-                $book.removeClass('bk-viewinside').addClass('bk-bookdefault');
+                // vi ska åt höger
+                $('.bk-bookdefault').removeClass('bk-bookdefault').addClass('bk-viewback');
+                $('.bk-viewinside').removeClass('bk-viewinside').addClass('bk-bookdefault');
             } else {
-                $book.removeClass('bk-bookdefault').addClass('bk-viewinside');
+                // vi ska åt vänster
+                $('.bk-bookdefault').removeClass('bk-bookdefault').addClass('bk-viewinside');
+                $('.bk-viewback').removeClass('bk-viewback').addClass('bk-bookdefault');
             }
         }, 0);
     }
